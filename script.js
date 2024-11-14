0a0e44f8cae38f6faf2aecf82d6e5b436202ab5e
Sardor336 <88594625+Sardor336@users.noreply.github.com>
2024-11-14 05:56:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,156 @@
 
+const suraNomlari = [
+    "Fotiha - Ochuvchi",
+    "Baqara - Sigir",
+    "Oli Imron - Imron oilasi",
+    "Niso - Ayollar",
+    "Moida - Dasturhon",
+    "An'om - Chorvalar",
+    "A'rof - Jannat bilan Do‘zaxni ajratuvchi devor",
+    "Anfol - O‘ljalar",
+    "Tavba - Tavba",
+    "Yunus - Yunus (nom) alayhissalam",
+    "Hud - Hud (nom) alayhissalam",
+    "Yusuf - Yusuf (nom) alayhissalam",
+    "Ra'd - Momaqaldiroq",
+    "Ibrohim - Ibrohim (nom) alayhissalam",
+    "Hijr - Madina va Shom ortasidagi vodiy",
+    "Nahl - Asalari",
+    "Isro - Tungi sayr",
+    "Kahf - G‘or",
+    "Maryam - Maryam (nom) roziAllahu anxo",
+    "Toho - Toxo harf",
+    "Anbiyo - Payg‘ambarlar",
+    "Haj - Haj",
+    "Muminun - Mo‘minlar",
+    "Nur - Nur",
+    "Furqon - Haq bilan nohaqni ajratib berguvchi",
+    "Shuaro - Shoirlar",
+    "Naml - Chumoli",
+    "Qasas - Qissa",
+    "Ankabut - O‘rgimchak",
+    "Rum - Rum (davlat nomi)",
+    "Luqman - Luqmon",
+    "Sajda - Sajda",
+    "Ahzab - Firqalar",
+    "Saba - Saba' (qabila nomi)",
+    "Fatir - Ilk Yaratguvchi",
+    "Yasin - Yasin, harf",
+    "Vas-saffat - Saf tortib turguvchilar",
+    "Sod - Sod, harf",
+    "Zumar - Guruhlar",
+    "G'ofir - Mag‘firat qilguvchi",
+    "Fussilat - Mufassal bayon qilingan",
+    "Sho'ro - Maslahat",
+    "Zuhruf - Zeb ziynat",
+    "Duxon - Tutun",
+    "Jasiya - Tiz cho‘kuvchilar",
+    "Ahqof - Qumtepalar",
+    "Muhammad - Muhammad solallahu alayhi vasallam",
+    "Fath - G'alaba",
+    "Hujurot - Hujralar",
+    "Qof - Qof, harf",
+    "Vazzariyat - Surib ketuvchi shamollar",
+    "Tur - Tur tog'i",
+    "Najm - Yulduz",
+    "Qomar - Oy",
+    "Rohman - Mehribon",
+    "Vaqea - Voqea",
+    "Hadid - Temir",
+    "Mujadala - Munozara",
+    "Hashr - Jamlab surgun qilish",
+    "Mumtahana - Imtihon qilinguvchi ayol",
+    "Saf - Saf, qator",
+    "Juma - Juma kuni",
+    "Munafiqun - Munofiqlar",
+    "Tag'obun - Bir birini aldash- ziyon keltirish kuni",
+    "Talaq - Taloq",
+    "Tahrim - Harom qilib olish",
+    "Mulk - Boylik",
+    "Qolam - Qalam, yozuv asbobi",
+    "Alhaqqo - Aniq ro‘y berguvchi",
+    "Maa'rij - Osmon qavatlari",
+    "Nuh - Nuh (nom) alayhisalam",
+    "Jin - Jinlar",
+    "Muzzammil - Kiyimlariga o‘ralib olgan zot",
+    "Muddassir - Liboslariga burkanib olgan zot",
+    "Qiyamat - Qiyamat",
+    "Inson - Inson",
+    "Mursalat - Yuborilgan shamollar",
+    "Naba - Habar",
+    "Naziyat - Jon olguvchi farishtalar",
+    "Abasa - Qoshini chimirdi, yuzini burishtirdi",
+    "Takvir - O‘ralish",
+    "Infitor - Yorilish",
+    "Mutaffifun - Boshqalar haqqidan urib qolguvchi",
+    "Inshiqoq - Yorilish",
+    "Buruj - Osmondagi burjlar",
+    "Toriq - Tungi yo‘lchi",
+    "Ala - Eng oliy zot",
+    "G'oshia - O‘rab olguvchi",
+    "Fajr - Tong",
+    "Balad - Shahar",
+    "Shams - Quyosh",
+    "Layl - Kecha, tun",
+    "Zuxo - Choshgoh vaqti",
+    "Sharh - Ochmoq, keng qilmoq",
+    "Tiyn - Anjir",
+    "Alaq - Lahta qon",
+    "Qodr - Qadrli kecha",
+    "Bayyina - Aniq hujjat",
+    "Zalzala - Zilzila",
+    "Adiyat - Chopqir otlar",
+    "Qoria - Qalblarni qattiq qo‘rqituvchi, qiyomat",
+    "Takasur - To‘plab ko‘paytirishga berilmoq",
+    "Asr - Asr namoz vaqti",
+    "Humaza - Bo‘htonchi",
+    "Fil - Fil, hayvon",
+    "Quroysh - Quraysh, qabila",
+    "Ma'un - Idish tavoq",
+    "Kavsar - Kavsar, Xovuz",
+    "Kafirun - Kofirlar",
+    "Nasr - Yordam",
+    "Masad - Pishiq tola",
+    "Ihlas - Ihlos qilish",
+    "Falaq - Tong",
+    "Nas - Odamlar"
+];
+
+const suraNomi = document.getElementById('sura');
+
+suraNomlari.forEach((key, index) => {
+    suraNomi.insertAdjacentHTML('beforeEnd', `<option value="${index + 1}">${index + 1} ${key}</option>`)
+})
 
 
+function showTafsiri() {
+    const sura = document.getElementById('sura').value;
+    const ayat = document.getElementById('ayat').value;
+    const tafsirContainer = document.getElementById('tafsir');
 
-function btnSearch() {
-    let input1 = [...document.querySelectorAll('input')]
-    let abtn = document.querySelector('form')
-    var xhr = new XMLHttpRequest()
-    xhr.open(
-        'GET',
-        'https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions/uzb-muhammadsodikmu.json',
-        true
-    )
-    xhr.send()
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState != 4) {
-            return
-        }
-
-        if (xhr.status === 200) {
-            // console.log(input1[1].value);
-            let userSura = +input1[0].value
-            let userOyat = +input1[1].value
-            // console.log('result', xhr.responseText)
-            // sura = JSON.parse(xhr.responseText).quran.userSura.chapter
-            // oyat = JSON.parse(xhr.responseText).quran.userOyat.verse
-            // if (JSON.parse(xhr.responseText).quran.chapter == userSura && JSON.parse(xhr.responseText).quran.verse == userOyat) {
-            //     text = JSON.parse(xhr.responseText).quran.text
-            //     console.log('result', JSON.parse(xhr.responseText).quran.text)
-
-            //     abtn.insertAdjacentHTML('afterEnd', `<p>${text}</p>`)
-            // }
-
-            console.log( userOyat);
-            console.log( userSura);
-
-            let lst = JSON.parse(xhr.responseText).quran
-            lst.forEach(key => {
-                // console.log(key);
-                if(key.chapter == userSura && key.verse == userOyat) {
-                    console.log('ishladi');
-                    console.log('result', key.text)
-                    abtn.insertAdjacentHTML('afterEnd', `<p>${key.text}</p>`)
+    fetch('./tafsir.json')
+        .then(response => response.json())
+        .then(data => {
+           
+            for(const key in data.quran) {
+                
+                if(data.quran[key].chapter == sura) {
+                    if(data.quran[key].verse == ayat) {
+                        console.log(data.quran[key].text);
+                        tafsirContainer.innerHTML = `<h3>Tafsir:</h3><p>${data.quran[key].text}</p>`;
+                    }else if(data.quran[key].verse < ayat){
+                        tafsirContainer.innerHTML = `<p>Tafsir topilmadi, ${ayat} oyat yo'q!!!.</p>`;
+                    }
                 }
-            });
+                
+            }
 
-            
-        } else {
-            console.log('err', xhr.responseText)
-        }
-    }
+        })
+        .catch(error => {
+            console.error('Xato yuz berdi:', error); 
+        });
 
-    // console.log('salom', typeof +input1.value);
+    // }
 }
 
-// const form = document.querySelector('.my')
 
-// console.log(form);
